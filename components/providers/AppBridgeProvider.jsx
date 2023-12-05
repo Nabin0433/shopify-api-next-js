6f0e0629f8cfc90bdfcda1d222249afc8857d793
nabin <nabin043@gmail.com>
2023-12-05 09:33:28 +0545

    first commit

--- components/providers/AppBridgeProvider.jsx
+++ components/providers/AppBridgeProvider.jsx
@@ -2,6 +2,8 @@ const AppBridgeProvider = ({ children }) => {
   if (typeof window !== "undefined") {
     const shop = window?.shopify?.config?.shop;
 
+    console.log(window.shopify);
+
     if (!shop) {
       return <p>No Shop Provided</p>;
     }
